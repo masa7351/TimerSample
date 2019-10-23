d6a40fdb6e670f4c4419717fa47ff29a89767832
Masanao Imai <masa7351@gmail.com>
2019-10-24 08:53:25 +0900

    First commit

---
+++ Pods/Target Support Files/Pods-TimerSample/Pods-TimerSample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TimerSample : NSObject
+@end
+@implementation PodsDummy_Pods_TimerSample
+@end
---
+++ TimerSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimerSample
+//
+//  Created by Masanao Imai on 2019/10/24.
+//  Copyright © 2019 Masanao Imai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
