3f4807521c7022c45f3d6f540588479d422cfa76
今井 真尚 <masanao_imai@waku-2.com>
2019-10-24 10:51:46 +0900

    Create CountDown Timer View Add .swiftlint Add README.md and LICENSE

---
+++ README.md
@@ -0,0 +1,21 @@
+Timer Sample
+====
+This is simple timer using [CombineTimer](https://github.com/masa7351/CombineTimer).
+
+## Demo
+
+|initial|running|pause|setTime|
+|:----|:----|:----|:----|
+|![](docs/images/initial.png)|![](docs/images/running.png)|![](docs/images/pause.png)|![](docs/images/setTime.png)|
+
+## Requirements & Support
+- Xcode 11.1
+- iOS 13.0 over
+
+## Licence
+
+[MIT](https://github.com/masa7351/setupHelper/blob/master/LICENSE)
+
+## Author
+
+[masa7351](https://github.com/masa7351)
--- TimerSample/ContentView.swift
+++ TimerSample/ContentView.swift
@@ -6,11 +6,61 @@
 //  Copyright © 2019 Masanao Imai. All rights reserved.
 //
 
+import CombineTimer
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject private var timer: CountDownTimer
+    @State private var isFirst: Bool = true
+    @State private var timeSelection: Int = 3
+    let selectableTimes: [Int] = [10, 20, 30, 60]
+    private var setupTime: Int {
+        selectableTimes[timeSelection]
+    }
+
+    init(timer: CountDownTimer = CountDownTimer(count: 60)) {
+        self.timer = timer
+    }
+
+    private var canResume: Bool {
+        return !isFirst && timer.hasTime
+    }
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            ZStack {
+                Form {
+                    Section {
+                        Text("Remaining time: \(timer.count)s")
+                    }
+                    if timer.isRunning {
+                        Button(action: {
+                            self.timer.stop()
+                        }, label: { Text("Stop") })
+                    } else {
+                        Picker(selection: $timeSelection, label: Text("Timer")) {
+                            ForEach(0 ..< selectableTimes.count) {
+                                Text("\(self.selectableTimes[$0])s")
+                            }
+                        }
+                        Section {
+                            if canResume {
+                                Button(action: {
+                                    self.timer.resume()
+                                }, label: { Text("Resume") })
+                            }
+                            Button(action: {
+                                self.isFirst.toggle()
+                                self.timer.start(self.setupTime)
+                            }, label: { Text("Start") })
+                        }
+                    }
+                }
+                if self.timer.isFinish {
+                    Text("Timer is finish.")
+                }
+            }.navigationBarTitle(Text("Cound Down"), displayMode: .inline)
+        }
     }
 }
 
